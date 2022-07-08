52d6fccecba4423a7b2f6c1571ed205272c9f191
agrael <lempionnewliving@gmail.com>
2022-07-08 13:20:16 +0300

    kopeiki

--- internal/models/models.go
+++ internal/models/models.go
@@ -2,6 +2,6 @@ package models
 
 // User is a bank user with some balance in wallet
 type User struct {
-	ID      int `json:"user_id"`
-	Balance int `json:"balance"`
+	ID      int     `json:"user_id"`
+	Balance float64 `json:"balance"`
 }
